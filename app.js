301f32f21453c0162404497b31120537f92ea35f
John Bettinger <johnbettinger3@gmail.com>
2023-07-08 22:04:25 -0500

    Using ES

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 // import pinecone client
-import {PineconeClient} from "@pinecone-database/pinecone"
+import {PineconeClient} from "@pinecone-database/pinecone";
 // import directory loader?
     // text loader?
     // PDF loader?
 // access dotenv
-require("dotenv").config(); // maybe change?
+import 'dotenv/config'        //require("dotenv").config(); // maybe change?
+console.log(process.env);
+
+
 // import other files
 
 // setup pinecone client
@@ -14,37 +17,38 @@ await client.init({
     environment: process.env.PINECONE_ENVIRONMENT,
 });
 
-const pineconeIndex = "exproy"
+const pineconeIndex = "exproy";
+const vectorDimensions = 1536;
 
 // following can be moved to another file if needed
 
 const createRequest = {
-    name: indexName,
-    dimension: dimensions,
+    name: pineconeIndex,
+    dimension: vectorDimensions,
     metric: "cosine"
 };
   
 await client.createIndex({ createRequest }); // brackets necessary?
 
 
-const express = require('express')
-const app = express()
-const path = require('path')
+// import {express} from "express"
+// const app = express()
+// import {path} from "path"
 
-app.all('*',(req,res) => {
-    res.status(404).send('<h1> Error 404, resource not found </h1>')
-})
+// app.all('*',(req,res) => {
+//     res.status(404).send('<h1> Error 404, resource not found </h1>')
+// })
 
 // for when the frontend has an html
 //app.use(express.static('./public'))
 
 //homepage
-app.get('/', (req,res) => {
-  res.status(200).send('Hello World')
-  //res.sendFile(path.resolve(__dirname, '../navbar-app/index.html')) //change file path
-})
+// app.get('/', (req,res) => {
+//   res.status(200).send('Hello World')
+//   //res.sendFile(path.resolve(__dirname, '../navbar-app/index.html')) //change file path
+// })
 
 
-app.listen(4000,() => {
-  console.log('listening on port 4000...')
-})
+// app.listen(4000,() => {
+//   console.log('listening on port 4000...')
+// })
